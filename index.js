69eda4e900649d1ef92aa11c9cbedbcd0c287a10
prabinnnn <prabinbhandari120@gmail.com>
2024-01-23 17:21:42 +0545

    add index

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const indexrouter = require("./router");
 const app = express();
+app.use(express.json());
 app.use("/", indexrouter);
+
 app.listen(8000, () => {
   console.log("app is running");
 });
--- router/blog.route.js
+++ router/blog.route.js
@@ -2,4 +2,8 @@ const router = require("express").Router();
 router.get("/:name", (req, res) => {
   res.json({ msg: "hello from blog" });
 });
+router.post("/", (req, res) => {
+  console.log(req.body);
+});
+
 module.exports = router;
