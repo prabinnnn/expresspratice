8e1b764e9664d82a1187d8551344f7298faafded
prabinnnn <prabinbhandari120@gmail.com>
2024-01-23 16:33:21 +0545

    add index1

--- index1.js
+++ index1.js
@@ -1,9 +1,11 @@
-const result=(solution) =>
-{
-    retrun solution;
+const arr1 = ["pasta", "pizza", "icecream"];
+const arr2 = ["pasta", "cake", "icecream"];
+const result = (solution) => {
+  return solution;
 };
-const itemfinder =(arr1,arr2,result)=>
-{
-    const answer=arr1.filter((food)=>arr2.includes(food));
-    return result(answer)
+const itemfinder = (arr1, arr2, result) => {
+  const answer = arr1.filter((food) => arr2.includes(food));
+  return result(answer);
 };
+const respone = itemfinder(arr1, arr2, result);
+console.log({ respone });
